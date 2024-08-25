b0c6b06e4105a4738c9fc5d477528658609efeb9
Peanutsu <adrianojunior1227@gmail.com>
2024-08-25 14:01:54 -0300

    adição da função derivada_implicita

--- derivada.py
+++ derivada.py
@@ -2,6 +2,7 @@ import sympy as smp
 from sympy import *
 import numpy as np
 import grafico
+import sympy as sp
 x,y,z,t = smp.symbols("x y z t", real = True)
 e = smp.E
 #function = input("Digite aqui sua z=f(x,y): ")
@@ -13,6 +14,7 @@ df_dy = smp.diff(f, y)
 df_dx_x0y0 = df_dx.subs({x:x0, y:y0})
 df_dy_x0y0 = df_dy.subs({x:x0, y:y0})
 f_x0y0 = f.subs({x:x0, y:y0})
+
 
 def plano_tangente(x0, y0 , f_x0y0):
     return f"z = {df_dx_x0y0*(x-x0) + df_dy_x0y0*(y-y0) + f_x0y0}"
@@ -32,6 +34,21 @@ def vetor_unitario(i,j):
         return [i/(i**2+j**2)**(1/2), j/(i**2+j**2)**(1/2)]
 def derivada_direcional():
     return (df_dx_x0y0*v[0]) + (df_dy_x0y0*v[1])
+#derivada implicita
+def derivada_implicita(equacao, var_dependente, var_independente):
+    # derivada parcial relação a V independente
+    independente = sp.diff(equacao, var_independente)
+
+    # derivada parcial relação a V dependente
+    dependente = sp.diff(equacao, var_dependente)
+
+    # derivada implícita de c2
+    dImplicita = -independente / dependente
+
+    return dImplicita
+#para mudar parametros da função derivada implicita, utilize a logica:
+#derivada_implicita(NomeFuncao, x, y)    'parametros: implícita de x em relaçap a y'
+
 
 print(f"P({x0}, {y0}, {f_x0y0})")
 print(df_dx)
@@ -40,4 +57,5 @@ print(plano_tangente(x0, y0, z))
 print(reta_normal(x0,y0,z))
 print(vetor_unitario(v[0],v[1]))
 print(derivada_direcional())
+print("Derivada implícita dz/dx:", derivada_implicita(f,y,x))
 grafico.grafico_funcao(f)
